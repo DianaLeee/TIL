54127558a3c9d4c834cf6c9426b72219eb877863
DianaLeee <yeonylee94@gmail.com>
2020-04-14 18:50:19 +0900

    Add Movie Description Card Component

---
+++ coding/movie-app/src/Movies.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+
+interface IMovie {
+  id: number;
+  year: number;
+  title: string;
+  summary: string;
+  poster: string;
+  genres: Array<string>;
+}
+
+const Movie = (movieInfo: IMovie) => {
+  return (
+    <div className="movie">
+      <img src={movieInfo.poster} alt={movieInfo.title} title={movieInfo.title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{movieInfo.title}</h3>
+        <h5 className="movie__year">{movieInfo.year}</h5>
+        <ul className="movie__genres">
+          {movieInfo.genres.map((genre, idx) => (
+            <li className="genres__genre" key={idx}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{movieInfo.summary}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
