091403349197e309ebd5fc3cbd9006610257e3eb
DianaLeee <yeonylee94@gmail.com>
2020-04-15 03:23:59 +0900

    Add Router & Set Link path

--- coding/movie-app/src/App.tsx
+++ coding/movie-app/src/App.tsx
@@ -1,57 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Movie from "./Movies";
-import styled from "styled-components";
-
-interface IMovieFromAPI {
-  id: number;
-  year: number;
-  title: string;
-  summary: string;
-  medium_cover_image: string;
-  genres: Array<string>;
-}
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMoviesFromAPI = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    setMovies(movies);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getMoviesFromAPI();
-  });
-
   return (
-    <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">Loading...</span>
-        </div>
-      ) : (
-        <div className="movies">
-          {movies.map((movie: IMovieFromAPI) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              poster={movie.medium_cover_image}
-              summary={movie.summary}
-              genres={movie.genres}
-            ></Movie>
-          ))}
-        </div>
-      )}
-    </section>
+    <BrowserRouter>
+      <Navigation />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </BrowserRouter>
   );
 };
 
