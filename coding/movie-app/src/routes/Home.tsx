a10ab1e3fe59267966823496eb87727a10067583
DianaLeee <yeonylee94@gmail.com>
2020-04-15 03:24:29 +0900

    Make component to route

---
+++ coding/movie-app/src/routes/Home.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Movie from "../components/Movies";
+import "./Home.css";
+
+interface IMovieFromAPI {
+  id: number;
+  year: number;
+  title: string;
+  summary: string;
+  medium_cover_image: string;
+  genres: Array<string>;
+}
+
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMoviesFromAPI = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    setMovies(movies);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMoviesFromAPI();
+  });
+
+  return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map((movie: IMovieFromAPI) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              poster={movie.medium_cover_image}
+              summary={movie.summary}
+              genres={movie.genres}
+            ></Movie>
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
