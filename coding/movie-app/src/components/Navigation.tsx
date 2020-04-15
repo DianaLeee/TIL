1e8768ec2dd1eb9f0f2de30a2d0a54b5fef92ead
DianaLeee <yeonylee94@gmail.com>
2020-04-15 23:19:53 +0900

    Add style on Navigation

--- coding/movie-app/src/components/Navigation.tsx
+++ coding/movie-app/src/components/Navigation.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <div>
+    <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
