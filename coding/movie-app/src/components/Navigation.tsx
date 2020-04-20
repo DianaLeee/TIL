30c277c7eff4fbaab02fd8694e1ab76016b3f16c
DianaLeee <yeonylee94@gmail.com>
2020-04-20 23:49:30 +0900

    Add active link style

--- coding/movie-app/src/components/Navigation.tsx
+++ coding/movie-app/src/components/Navigation.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navigation.css";
 
+const activeStyle = {
+  background: "linear-gradient(to left, rgba(0,0,0,0) 85%, #02e3f717 97%, #02e4f7)",
+  color: "#555555",
+};
 const Navigation = () => {
   return (
     <div className="nav">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        <i className="far fa-eye"></i>
+        Discover
+      </NavLink>
+      <NavLink to="/about" activeStyle={activeStyle}>
+        <i className="fas fa-users"></i>
+        About
+      </NavLink>
     </div>
   );
 };
