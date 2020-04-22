3c3a5cb10a2402364923f1d2842d780c5b06f84d
DianaLeee <yeonylee94@gmail.com>
2020-04-22 23:27:46 +0900

    Edit CSS for Responsive Web

--- coding/movie-app/src/components/Navigation.tsx
+++ coding/movie-app/src/components/Navigation.tsx
@@ -1,22 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./Navigation.css";
 
 const activeStyle = {
-  background: "linear-gradient(to left, rgba(0,0,0,0) 85%, #02e3f717 97%, #02e4f7)",
-  color: "#555555",
+  color: "#93acff",
 };
+
 const Navigation = () => {
+  const [isNavClicked, setIsNavClicked] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  const handleMediaQueryChange = (mediaQuery: any) => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+
+    setIsNavClicked(false);
+  };
+
+  const toggleNav = () => {
+    setIsNavClicked(!isNavClicked);
+  };
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <div className="nav">
-      <NavLink to="/" activeStyle={activeStyle} exact>
-        <i className="far fa-eye"></i>
-        Discover
-      </NavLink>
-      <NavLink to="/about" activeStyle={activeStyle}>
-        <i className="fas fa-users"></i>
-        About
-      </NavLink>
+    <div className="header">
+      <div className="header__wrapper">
+        {isSmallScreen ? (
+          <div>
+            <i className="fas fa-bars" onClick={toggleNav} />
+            {isNavClicked ? (
+              <div className="mobile__header">
+                <i className="fas fa-times" onClick={toggleNav}></i>
+                <NavLink to="/" activeStyle={activeStyle} exact onClick={toggleNav}>
+                  Discover
+                </NavLink>
+                <NavLink to="/about" activeStyle={activeStyle} onClick={toggleNav}>
+                  About
+                </NavLink>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        ) : (
+          <div className="desktop__header">
+            <NavLink to="/" activeStyle={activeStyle} exact>
+              Discover
+            </NavLink>
+            <NavLink to="/about" activeStyle={activeStyle}>
+              About
+            </NavLink>
+          </div>
+        )}
+        {/* <NavLink to="/" activeStyle={activeStyle} exact>
+          Discover
+        </NavLink>
+        <NavLink to="/about" activeStyle={activeStyle}>
+          About
+        </NavLink> */}
+      </div>
     </div>
   );
 };
