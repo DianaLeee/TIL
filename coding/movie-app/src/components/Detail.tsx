cf9692d1a984cb889aa7cd13fa35278965396e06
DianaLeee <yeonylee94@gmail.com>
2020-04-24 20:32:19 +0900

    Add Detail movie page

--- coding/movie-app/src/components/Detail.tsx
+++ coding/movie-app/src/components/Detail.tsx
@@ -1,6 +1,6 @@
 import React, { Component, useEffect } from "react";
 import { RouteComponentProps } from "react-router-dom";
-
+import "./Detail.css";
 interface IMovie {
   id: number;
   year: number;
@@ -18,11 +18,39 @@ const Detail = (props: any) => {
 
     if (location.state === undefined) {
       history.push("/");
+    } else {
+      document.title = location.state.title;
     }
   });
 
   if (location.state) {
-    return <span>{location.state.title}</span>;
+    const movieInfo: IMovie = location.state;
+
+    return (
+      <section className="container">
+        <div className="detail-infos">
+          <div className="detail-info">
+            <div className="image__wrapper">
+              <img src={movieInfo.poster} />
+            </div>
+            <div className="text__wrapper">
+              <h1>{movieInfo.title}</h1>
+              <ul className="detail__genres">
+                {movieInfo.genres.map((genre, idx) => (
+                  <li>{genre}</li>
+                ))}
+              </ul>
+              <p className="detail__summary">
+                <h1>Summary</h1>
+                {movieInfo.summary}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+
+    // <span>{location.state.title}</span>;
   } else {
     return null;
   }
